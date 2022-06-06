5608350262c798304a0140898fa9913e4c064faf
Shayan <mshayan02@gmail.com>
2022-06-06 17:24:14 +0530

    Optimisations

---
+++ serviceWorker.js
@@ -0,0 +1,24 @@
+const staticGlitterCurls = "Glitter-Curls"
+const assets = [
+  "/",
+  "/index.html",
+  "/css/main.css",
+  "/js/main.js",
+  "/images/anjum.webp",
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticGlitterCurls).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request);
+      })
+    );
+  });
