9279dc330eda8329e5c1797b3e87eaee0e383653
Shayan <mshayan02@gmail.com>
2022-06-07 01:12:00 +0530

    PWA Bug Fixes

--- serviceWorker.js
+++ serviceWorker.js
@@ -15,10 +15,12 @@ self.addEventListener("install", installEvent => {
   )
 })
 
+/*
 self.addEventListener("fetch", fetchEvent => {
     fetchEvent.respondWith(
       caches.match(fetchEvent.request).then(res => {
-        return res || fetch(fetchEvent.request);
+        return res// || fetch(fetchEvent.request);
       })
     );
   });
+  */
